1ce728ce62e6765beb8cd6b8e3ffccda65ffeb9d
PatatasDelPapa <43626645+PatatasDelPapa@users.noreply.github.com>
2021-01-13 12:36:55 -0300

    Añadida lista de proximos videos

--- src/render_file_watcher.js
+++ src/render_file_watcher.js
@@ -19,30 +19,31 @@ startBtn.addEventListener('click', function(e) {
         if (path) {
             flag = true;
             listaReproduccion = [];
-            console.log(path);
+            console.log('cleaning listaReproduccion');
+            // console.log(path);
             const watcher = chokidar.watch(path+"/*.mp4", { persistent: true });
             watcher
-            .on('add', path => {
-                console.log(`File ${path} has been added`)
-                videos.add(path);
-                listaReproduccion.push(path);
-                console.log(listaReproduccion);
+            .on('add', video_file => {
+                console.log(`File ${video_file} has been added`)
+                videos.add(video_file);
+                listaReproduccion.push(video_file);
+                // console.log(listaReproduccion);
             })
-            .on('unlink', path => {
-                console.log(`File ${path} has been removed`)
-                videos.delete(path);
+            .on('unlink', video_file => {
+                console.log(`File ${video_file} has been removed`)
+                videos.delete(video_file);
                 listaReproduccion = [...videos];
-                console.log(listaReproduccion);
+                // console.log(listaReproduccion);
             });
             
             // More possible events.
             watcher
-            .on('addDir', path => console.log(`Directory ${path} has been added`))
-            .on('unlinkDir', path => console.log(`Directory ${path} has been removed`))
-            .on('change', path => console.log(`File ${path} has been changed`))
+            .on('addDir', video_file => console.log(`Directory ${video_file} has been added`))
+            .on('unlinkDir', video_file => console.log(`Directory ${video_file} has been removed`))
+            .on('change', video_file => console.log(`File ${video_file} has been changed`))
             .on('error', error => console.log(`Watcher error: ${error}`))
             .on('ready', () => console.log('Initial scan complete. Ready for changes'))
-
+            
         }
     }
 
@@ -60,17 +61,48 @@ watchBtn.addEventListener('click', function(e) {
         } else {
             console.log("No se detectaron archivos mp4 en la carpeta seleccionada");
         }
+    } else {
+        console.log(listaReproduccion);
     }
 });
+
+const listContainer = document.getElementById('listDiv');
+const listElement = document.createElement('ol');
+const listTitle = document.createElement('h2');
+listTitle.innerText = "Siguientes videos";
+
+function makeList() {
+    
+    // Set up a loop that goes through the items in listItems one at a time
+    listContainer.innerHTML = "";
+    listElement.innerHTML = "";
+    numberOfListItems = listaReproduccion.length;
+    let listItem;
+    listContainer.appendChild(listTitle);
+    listContainer.appendChild(listElement);
+
+    for (i = 0; i < numberOfListItems; ++i) {
+        // create an item for each one
+        listItem = document.createElement('li');
+
+        // Add the item text
+        listItem.innerHTML = listaReproduccion[i];
+
+        // Add listItem to the listElement
+        listElement.appendChild(listItem);
+    }
+}
 
 async function playVideo() {
     console.log(listaReproduccion);
     video = listaReproduccion.shift();
-    console.log(`The video is ${video}`);
+    // console.log(`The video is ${video}`);
     listaReproduccion.push(video);
     // console.log(videos);
     videoElement.src = video;
+    makeList();
     await videoElement.play();
 }
 
 videoElement.onended = playVideo;
+
